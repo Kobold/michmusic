58f9f5e009e1f2bad6429f10142daf82254528b7
Andy Kish <agkish@gmail.com>
2009-09-08 22:38:13 -0400

    added basic listing of songs and display of metadata

--- main.clj
+++ main.clj
@@ -1,14 +1,21 @@
 (ns michmusic.main
   (:use compojure)
-  (:import [java.io File]))
+  (:import [java.io File]
+           [org.jaudiotagger.audio AudioFileIO]))
 
-(def *music-directory* "/Users/kobold/Music")
+(def *music-directory* (File. "/Users/kobold/Music"))
+
+(defn extract-metadata [file]
+  (let [tag (.getTag (AudioFileIO/read file))]
+    {:title  (.getFirstTitle  tag)
+     :album  (.getFirstAlbum  tag)
+     :artist (.getFirstArtist tag)
+     :path   (.getPath file)}))
 
 (defn list-mp3 []
-  (let [is-mp3? (fn [file] (.. file getName (endsWith ".mp3")))
-        dir (File. *music-directory*)]
-    (assert (.isDirectory dir))
-    (filter is-mp3? (file-seq dir))))
+  (let [is-mp3? (fn [file] (and (.isFile file)
+                                (.. file getName (endsWith ".mp3"))))]
+    (filter is-mp3? (file-seq *music-directory*))))
 
 (defn html-doc 
   [title & body] 
@@ -23,11 +30,15 @@
        [:a {:href "/"} "Home"]]]
      body]])) 
 
+(defn song-table [song]
+  [:div.song
+   (for [[k v] song]
+     [:p (str (name k) ": " v)])])
+
 (def mp3-page
      (html-doc "MP3s"
-       [:ul
-        (map #(vector :li %)
-             (list-mp3))]))
+       (map #(song-table (extract-metadata %))
+            (take 10 (list-mp3)))))
 
 (defroutes webservice
   (GET "/" mp3-page)) 
