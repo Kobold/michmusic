54167f5a290a5b526c6e334745b1c4f037aef030
Andy Kish <agkish@gmail.com>
2009-09-27 17:25:23 -0400

    turned off jaudiotaggers high level of logging

--- serve.clj
+++ serve.clj
@@ -1,7 +1,14 @@
 (ns michmusic.serve
   (:use compojure
         michmusic.database
-        michmusic.main))
+        michmusic.main)
+  (:import [java.util.logging LogManager]
+           [java.io StringBufferInputStream]))
+
+; disable jaudiotagger's crazy logging
+(let [stream (StringBufferInputStream. "org.jaudiotagger.level = OFF")]
+  (.. (LogManager/getLogManager)
+      (readConfiguration stream)))
 
 (run-server {:port 8080} 
             "/*"
