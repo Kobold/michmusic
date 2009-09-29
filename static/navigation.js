1dd072b6be3022f2c8f341c4f9dd24d2b3a8599a
Andy Kish <agkish@gmail.com>
2009-09-29 04:09:02 -0400

    fixed broken-ness when the server couldn't connect to last.fm

--- static/navigation.js
+++ static/navigation.js
@@ -1,6 +1,7 @@
 // sets up navigation between artists and the music player
 
 var oneBit = new OneBit('/static/external/1bit.swf');
+oneBit.specify('playerSize', '11');
 
 function artist_selected(event) {
   $.get($(this).val(), {}, function(data) {
