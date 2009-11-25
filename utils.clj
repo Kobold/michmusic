be13c8430544874309183405d3b83a5394ea048c
Andy Kish <agkish@gmail.com>
2009-11-24 23:02:53 -0500

    added a function to zip files nicely

--- utils.clj
+++ utils.clj
@@ -1,7 +1,26 @@
 (ns michmusic.utils
-  (:use [clojure.contrib.duck-streams :only (to-byte-array)]
+  (:require [clojure.contrib.str-utils2 :as str-utils2])
+  (:use [clojure.contrib.duck-streams :only (copy to-byte-array)]
+        [clojure.contrib.java-utils :only (file)]
         [clojure.contrib.str-utils :only (str-join)])
-  (:import [java.security MessageDigest]))
+  (:import [java.security MessageDigest]
+           [java.util.zip ZipEntry ZipOutputStream]))
+
+(defn- song-filename
+  "Converts a song struct to a filename appropriate for the mp3."
+  [song]
+  (let [escaped-title (str-utils2/replace (song :title) "/" "_")]
+    (str (song :track) " " escaped-title ".mp3")))
+
+(defn zip-files
+  "Given a list of songs, dumps them zipped into ostream."
+  [artist album songs ostream]
+  (let [dir-name (str artist " - " album "/")]
+    (with-open [out (ZipOutputStream. ostream)]
+      (doseq [song songs]
+        (.putNextEntry out (ZipEntry. (str dir-name (song-filename song))))
+        (copy (file (song :path)) out)
+        (.closeEntry out)))))
 
 (defn sha-1
   "Generates a SHA-1 hash of the given input plaintext.
